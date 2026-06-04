cbdb04edce95d7f328a8a624afd30696bd3e8766
sanjayguwaju <sanjayguwaju@gmail.com>
2026-06-04 15:16:53 +0545

    feat: add @payloadcms/plugin-cloud-storage dependency and update importMap configuration

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -34,6 +34,7 @@ import { AdminDashboardLayout as AdminDashboardLayout_d2e5e8cdcf265e3c61c4d68316
 import { ReportsViewLayout as ReportsViewLayout_a48564f2dc9ce7d42ff8095adeb773ee } from '@/components/Reports/ReportsViewLayout'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
+/** @type import('payload').ImportMap */
 export const importMap = {
   "@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell": RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e,
   "@payloadcms/richtext-lexical/rsc#RscEntryLexicalField": RscEntryLexicalField_44fe37237e0ebf4470c9990d8cb7b07e,
